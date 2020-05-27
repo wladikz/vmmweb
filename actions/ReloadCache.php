e8c3ac06b4908763a133485797a1dcc70ca4672c
wladikz@hotmail.com <wladikz@hotmail.com>
2020-05-27 14:31:42 +0300

    Update Service Templates from DB + parameters in query for injection protection

--- actions/ReloadCache.php
+++ actions/ReloadCache.php
@@ -29,8 +29,13 @@
         } 
         $vms=array();
         try {
-            $sql = "SELECT * FROM VMCache WHERE Owner='".$user."' OR UserRole='".$userRole."' OR GrantedToList like '%|".$userRole."|%'";
-            $result = $conn->query($sql);
+            $sql = "SELECT * FROM VMCache WHERE Owner=:user OR UserRole=:userRole OR GrantedToList like :userRole1";
+            $params = array(
+                "user" => $user,
+                "userRole" => $userRole,
+                "userRole1" =>"%|".$userRole."|%"
+            );
+            $result = $conn->query($sql,$params);
             if (is_array($result) &&  count($result) > 0) {
                 // output data of each row
                 foreach($result as $rowData) {
@@ -80,8 +85,13 @@
         } 
         $vms=array();
         try {
-            $sql = "SELECT * FROM SvcCache WHERE Owner='".$user."' OR UserRole='".$userRole."' OR GrantedToList like '%|".$userRole."|%'";
-            $result = $conn->query($sql);
+            $sql = "SELECT * FROM SvcCache WHERE Owner=:user OR UserRole=:userRole OR GrantedToList like :userRole1";
+            $params = array(
+                "user" => $user,
+                "userRole" => $userRole,
+                "userRole1" =>"%|".$userRole."|%"
+            );
+            $result = $conn->query($sql,$params);            
             if (is_array($result) &&  count($result) > 0) {
                 // output data of each row
                 foreach($result as $rowData) {
@@ -100,6 +110,54 @@
             return FALSE;
         }
     } 
+    function UpdateServiceTemplates() {
+        if (isset($_SESSION["username"]) && !empty($_SESSION["username"])) {
+            $user=$_SESSION["username"];
+        } else {
+            return FALSE;    
+        }
+        if (isset($_SESSION["userRole"]) && !empty($_SESSION["userRole"])) {
+            $userRole=$_SESSION["userRole"];
+        } else {
+            return FALSE;    
+        }
+        if (isset($_SESSION["ServiceTemplate"])) {
+            $curVMs=$_SESSION["ServiceTemplate"];
+        } else {
+            $curVMs=array();
+        }
+        $conn = new PDOWrapper\DB(CacheDbServer, CacheDbDatabase, CacheDbUser, CacheDbPassword);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        $vms=array();
+        try {
+            $sql = "SELECT * FROM SvcTmplCache WHERE Owner=:user OR UserRole=:userRole OR GrantedToList like :userRole1";
+            $params = array(
+                "user" => $user,
+                "userRole" => $userRole,
+                "userRole1" =>"%|".$userRole."|%"
+            );
+            $result = $conn->query($sql,$params);
+            if (is_array($result) &&  count($result) > 0) {
+                // output data of each row
+                foreach($result as $rowData) {
+                    $vm=json_decode($rowData["Data"]);
+                    $vm->LastChanged=strtotime($rowData["LastChanged"]);
+                    $vms[] = $vm;
+                }
+            }            
+        } finally {
+            $conn->CloseConnection();
+        }
+        if ($vms != $curVMs) {
+            $_SESSION["VMs"]=$vms;
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
     function GetServices(bool $reload=FALSE) {
         if (isset($_SESSION["Services"]) && !$reload) {
             return $_SESSION["Services"];
@@ -119,10 +177,12 @@
     if (str_replace('\\', '/', __FILE__) == $_SERVER['SCRIPT_FILENAME']) {
         $vmsUpdated=UpdateVMs() ? 'true' : 'false';
         $svcUpdated=UpdateServices() ? 'true' : 'false';
+        $svcTUpdated=UpdateServiceTemplates() ? 'true' : 'false';
         $xml = new SimpleXMLElement('<xml version="1.0"/>');
         $xml->addAttribute('encoding',"iso-8859-1");
         $item = $xml->addChild('VMsUpdated',$vmsUpdated);
         $item = $xml->addChild('SvcUpdated',$svcUpdated);
+        $item = $xml->addChild('SvcTUpdated',$svcTUpdated);
         Header('Content-type: text/xml');
         $tmp=$xml->asXML();
         print($xml->asXML());
